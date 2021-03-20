a66d2cc8ae223f18d83f183050cd8c1c1d98cbd0
= <vamshi.a4u@gmail.com>
2021-03-20 14:38:30 +0530

    db

--- Matrix/settings.py
+++ Matrix/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'Matrix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'd1396atferpdg5',
-#         'HOST':'ec2-3-91-127-228.compute-1.amazonaws.com',
-#         'PORT':'5432',
-#         'USER':'bfxwwyahkgcsoi',
-#         'PASSWORD':'0c894c54d86fc008b0eb8f7f4d99748667a75c420cd95d0ce4b75b0ce2480527'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd1396atferpdg5',
+        'HOST':'ec2-3-91-127-228.compute-1.amazonaws.com',
+        'PORT':'5432',
+        'USER':'bfxwwyahkgcsoi',
+        'PASSWORD':'0c894c54d86fc008b0eb8f7f4d99748667a75c420cd95d0ce4b75b0ce2480527'
+    }
+}
 
 
 # Password validation
