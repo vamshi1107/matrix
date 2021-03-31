783d271187b1f050af465735b7ee33f7ff8a90fe
= <vamshi.a4u@gmail.com>
2021-03-31 15:46:11 +0530

    table

--- sub/models.py
+++ sub/models.py
@@ -14,7 +14,7 @@ class member(models.Model):
         name=models.CharField(max_length=100)
         username=models.CharField(max_length=100)
         password=models.CharField(max_length=30)
-        email=models.CharField(max_length=200)
+        email=models.EmailField(max_length=200)
 
         def __str__(self):
                 return self.name
